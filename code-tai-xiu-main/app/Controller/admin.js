7fc939ba0ffcb15e6bf8b45f6e330ce4c3d20e63
ghlovenhuy <huytngtb023590@gmail.com>
2026-04-21 08:33:12 +0700

    Delete code-tai-xiu-main directory

--- code-tai-xiu-main/app/Controller/admin.js
+++
@@ -1,8 +0,0 @@
-let admin = require('./Admin/messenger.js')
-module.exports = function(client,data)
-{
-    if(!!data.admin) 
-    {
-        admin(client,data.admin);
-    }
-}
--- code-tai-xiu-main/app/Model/VXMM/cuoc.js
+++
@@ -1,3 +0,0 @@
-let cuoc = [];
-
-module.exports = cuoc;
